6cef43a7e12f1b5e52553adf27c5c34e4ab1b4c4
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-04-29 17:31:35 +0000

    Added an icons util

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,8 @@ const config = {
 	
 	kit: {
 		alias: {
-			$utils: "src/utils"
+			$utils: "src/utils",
+			$utils: "src/icons"
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
