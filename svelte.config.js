306ef6bde2a86ed13d63f59237449db54cc858b9
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-04-29 16:40:35 +0000

    Added an alias for utils

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,11 @@ const config = {
 		vitePreprocess(),
 		mdsvex(mdsvexConfig)
 	],
-
+	
 	kit: {
+		alias: {
+			$utils: "src/utils"
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
