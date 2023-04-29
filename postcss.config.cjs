c456f2d30db44cf143112a5de3625c86eb75ca2c
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-04-29 20:59:34 +0000

    added post css preset env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,16 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const postcssPresetEnv = require("postcss-preset-env");
 
 const config = {
 	plugins: [
+		require('tailwindcss/nesting')(require('postcss-nesting')),
+		postcssPresetEnv({
+			stage: 1,
+			features: {
+				'nesting-rules': false
+			}
+		}),
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
