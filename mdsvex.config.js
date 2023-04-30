ce5edfa957e8f5f981195a85a5242655cad1e038
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-04-30 00:12:37 +0000

    Added a list of remark plugins

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,16 +1,24 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 
 import remark_slug from "remark-slug";
+import remark_breaks from "remark-breaks";
+import remark_emoji from "remark-emoji";
+import remark_gfm from "remark-gfm";
 
 const config = defineConfig({
-	extensions: ['.svelte.md', '.md', '.svx'],
+	extensions: ['.md'],
 
 	smartypants: {
 		dashes: 'oldschool'
 	},
 
 	remarkPlugins: [
-		remark_slug
+		remark_slug,
+		remark_breaks,
+		remark_gfm,
+		[remark_emoji, {
+			accessible: true
+		}]
 	],
 	rehypePlugins: []
 });
