a85f35bc4680742dc19c7ebac524b0fb161fc40f
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-05-14 23:16:40 +0000

    Added controls

--- remarkPlugins/mermaid/index.js
+++ remarkPlugins/mermaid/index.js
@@ -26,15 +26,34 @@ const plugin = () => async (tree) =>{
             await run(tempFile, outputFile);
 
             const mermaidSvg = fs.readFileSync(outputFile,'utf-8');
+            const mermaidElement = { type: 'html', value: mermaidSvg };
+            const controls = {
+                type: 'html',
+                value: `
+                <div class='mermaid-bottom'>
+                <div class='mermaid-controls'>
+                    <button class='mermaid-button' data-action='zoom-in' aria-label='Zoom in'><svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21 21L15.803 15.803M15.803 15.803C17.2096 14.3965 17.9998 12.4887 17.9998 10.4995C17.9998 8.51035 17.2096 6.60262 15.803 5.19605C14.3965 3.78947 12.4887 2.99927 10.4995 2.99927C8.51035 2.99927 6.60262 3.78947 5.19605 5.19605C3.78947 6.60262 2.99927 8.51035 2.99927 10.4995C2.99927 12.4887 3.78947 14.3965 5.19605 15.803C6.60262 17.2096 8.51035 17.9998 10.4995 17.9998C12.4887 17.9998 14.3965 17.2096 15.803 15.803ZM10.5 7.50005V13.5M13.5 10.5H7.50005" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    </button>
+                    <button class='mermaid-button' data-action='reset-zoom' araia-label='Reset zoom'><svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16.023 9.34794H21.015L17.834 6.16494C16.8099 5.14081 15.5342 4.40433 14.1352 4.02955C12.7362 3.65476 11.2632 3.65487 9.86428 4.02986C8.46534 4.40486 7.18977 5.14153 6.16581 6.16581C5.14184 7.19009 4.40557 8.46588 4.03101 9.86494M2.98501 19.6439V14.6519M2.98501 14.6519H7.97701M2.98501 14.6519L6.16501 17.8349C7.18912 18.8591 8.4648 19.5955 9.8638 19.9703C11.2628 20.3451 12.7358 20.345 14.1347 19.97C15.5337 19.595 16.8092 18.8584 17.8332 17.8341C18.8572 16.8098 19.5934 15.534 19.968 14.1349M21.015 4.35594V9.34594" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    </button>
+                    <button class='mermaid-button' data-action='zoom-out' aria-label='Zoom out'><svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21 21L15.803 15.803M15.803 15.803C17.2096 14.3965 17.9998 12.4887 17.9998 10.4995C17.9998 8.51035 17.2096 6.60262 15.803 5.19605C14.3965 3.78947 12.4887 2.99927 10.4995 2.99927C8.51035 2.99927 6.60262 3.78947 5.19605 5.19605C3.78947 6.60262 2.99927 8.51035 2.99927 10.4995C2.99927 12.4887 3.78947 14.3965 5.19605 15.803C6.60262 17.2096 8.51035 17.9998 10.4995 17.9998C12.4887 17.9998 14.3965 17.2096 15.803 15.803ZM13.5 10.5H7.50005" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    </button>
+                </div>
+                </div>
+                `
+            }
             const mainWrapper = {
                 type: 'div',
-                data: { hProperties: { class: 'mermaid' } },
-                children: [{type:'html', value: mermaidSvg}]
+                data: { hProperties: { class: 'mermaid-diagram' } },
+                children: [mermaidElement,controls]
               };
             parent.children[index] = mainWrapper;
-
-            fs.unlinkSync(tempFile);
-            fs.unlinkSync(outputFile)
             resolve();
         });
         promises.push(promise);
@@ -43,6 +62,5 @@ const plugin = () => async (tree) =>{
 
     await Promise.all(promises);
 }
-
 
 export default plugin;
