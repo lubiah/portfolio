0eb921115c70b8403c67c064f299a6c72da6e95f
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-05-15 06:52:14 +0000

    Fixed zoom

--- src/routes/blog/[slug=slug]/panZoom.ts
+++ src/routes/blog/[slug=slug]/panZoom.ts
@@ -4,7 +4,7 @@ const plugin = (node: HTMLElement) =>  {
     const svgElement: SVGElement = node.querySelector("svg")!;
     const resetZoomButton: HTMLElement = node.querySelector('[data-action=reset-zoom]')!;
     const zoomInButton: HTMLElement = node.querySelector('[data-action=zoom-in]')!;
-    const zoomOutButton: HTMLElement = node.querySelector('[data-action]=zoom-out')!;
+    const zoomOutButton: HTMLElement = node.querySelector('[data-action=zoom-out]')!;
 
     const zoom = panZoom(svgElement,{
         fit: true,
