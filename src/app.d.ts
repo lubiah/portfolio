0ec354861c92a447c26538db9e36a82df14ee998
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-05-16 21:10:33 +0000

    Added types for svg

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,46 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+declare module '*.svg?component' {
+	import type { ComponentType, SvelteComponentTyped } from 'svelte'
+	import type { SVGAttributes } from 'svelte/elements'
+  
+	const content: ComponentType<
+	  SvelteComponentTyped<SVGAttributes<SVGSVGElement>>
+	>
+  
+	export default content
+  }
+  
+  declare module '*.svg?src' {
+	const content: string
+	export default content
+  }
+  
+  declare module '*.svg?url' {
+	const content: string
+	export default content
+  }
+  
+  declare module '*.svg?dataurl' {
+	const content: string
+	export default content
+  }
+  
+  declare module '*.svg?dataurl=base64' {
+	const content: string
+	export default content
+  }
+  
+  declare module '*.svg?dataurl=enc' {
+	const content: string
+	export default content
+  }
+  
+  declare module '*.svg?dataurl=unenc' {
+	const content: string
+	export default content
+  }
 declare global {
 	namespace App {
 		// interface Error {}
