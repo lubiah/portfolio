49f1a17ec715fed83c857fa59501081346a32e35
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-05-16 20:39:50 +0000

    Removed reference to the reading-time util

--- src/routes/blog/[slug=slug]/+page.server.ts
+++ src/routes/blog/[slug=slug]/+page.server.ts
@@ -3,14 +3,11 @@ import type { PageServerLoad } from './$types';
 export const load = (async ({ params }) => {
 	const { slug } = params;
 	const article = (await import(`../content/${slug}/index.md`)).default.render()['html'];
-	const readingTimeModule = (await import('$utils/reading-time')).default;
-	const readingTime = readingTimeModule(article).text;
 	const tocModule = (await import('$utils/toc')).default;
 	const toc = tocModule(article);
 
 
 	return {
-		readingTime,
 		toc
 	};
 }) satisfies PageServerLoad;
