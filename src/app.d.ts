5dbd4c81181cf808280ebe87690093c5877dcd80
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-05-23 23:45:58 +0000

    Removed svg types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,55 +1,5 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-
-declare module '*.svg?component' {
-	import type { ComponentType, SvelteComponentTyped } from 'svelte'
-	import type { SVGAttributes } from 'svelte/elements'
-  
-	const content: ComponentType<
-	  SvelteComponentTyped<SVGAttributes<SVGSVGElement>>
-	>
-  
-	export default content
-  }
-  
-  declare module '*.svg?src' {
-	const content: string
-	export default content
-  }
-  
-  declare module '*.svg?url' {
-	const content: string
-	export default content
-  }
-  
-  declare module '*.svg?dataurl' {
-	const content: string
-	export default content
-  }
-  
-  declare module '*.svg?dataurl=base64' {
-	const content: string
-	export default content
-  }
-  
-  declare module '*.svg?dataurl=enc' {
-	const content: string
-	export default content
-  }
-  
-  declare module '*.svg?dataurl=unenc' {
-	const content: string
-	export default content
-  }
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
-}
-
 declare interface ArticleMeta {
 	title: string;
 	description: string;
@@ -66,4 +16,5 @@ declare interface ArticleMeta {
 	};
 	draft: true
 }
+
 
