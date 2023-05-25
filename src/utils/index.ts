8c222438992765f490bae59b80ff59b8f6ba2596
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-05-25 16:56:48 +0000

    added a sleep util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -39,3 +39,12 @@ export const encodeHtml = (string: string): string => {
       return `&#${i.charCodeAt(0)};`;
   });
 };
+
+
+/**
+ * Pauses the execution for the specified number of milliseconds.
+ * @param ms The number of milliseconds to sleep.
+ * @returns A Promise that resolves after the specified time has elapsed.
+ */
+export const sleep = (ms: number): Promise<void> =>
+  new Promise(resolve => setTimeout(resolve, ms));
