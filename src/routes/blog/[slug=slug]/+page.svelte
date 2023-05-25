204a2fd69a8cb422dbce937cb75c2bbbe327fa5c
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-05-25 16:56:39 +0000

    Removed grids

--- src/routes/blog/[slug=slug]/+page.svelte
+++ src/routes/blog/[slug=slug]/+page.svelte
@@ -30,18 +30,21 @@
 	description = {SEO.description}
 />
 
-<main class="max-w-screen-laptop mx-auto min-w-0">
-	<h1 class="mb-12 tablet:mb-16 laptop:mb-20 laptop:!col-[1/-1] mx-4 laptop:mx-0">
+<main class="max-w-screen-laptop mx-auto px-4 tablet:px-0">
+	<h1 class="mt-0 mb-12 tablet:mb-16 laptop:mb-20">
 		{data.metadata.title}
 	</h1>
+
+	<div class="-ml-4 -mr-4"> 
 	<img
 		src={data.metadata.image}
 		alt=""
 		loading="eager"
-		class="w-full h-auto max-h-[250px] tablet:max-h-[400px] laptop:object-cover mb-6 tablet:mb-8 laptop:mb-10 !col-[1/-1]"
+		class="w-full h-auto max-h-[250px] tablet:max-h-[400px] laptop:object-cover mb-6 tablet:mb-8 laptop:mb-10"
 	/>
+	</div>
 	<ul
-		class="text-gray-500 mb-6 tablet:mb-8 laptop:mb-10 text-xs laptop:text-base flex flex-col tablet:flex-row gap-2 tablet:gap-x-6 mx-4 laptop:mx-0 p-0"
+		class="text-gray-500 mb-6 tablet:mb-8 laptop:mb-10 text-xs laptop:text-base flex flex-col tablet:flex-row gap-2 tablet:gap-x-6 laptop:mx-0 p-0"
 	>
 		<li class="flex items-center gap-x-1">
 			<ClockIcon height="16" width="16" /><span>{data.metadata.readingTime.text}</span>
@@ -58,7 +61,7 @@
 		</li>
 	</ul>
 	<div class="laptop:flex justify-between">
-		<article class='prose'>
+		<article>
 			<svelte:component this={data.component} />
 		</article>
 		{#if data.toc}
@@ -74,21 +77,19 @@
 <Progress color="#ED0101"/>
 
 <style lang="postcss">
+
 	article {
-		@apply grid grid-cols-[1fr_min(65ch,calc(100%-32px))_1fr] laptop:grid-cols-[65ch_1fr] mb-[60px];
+		@apply max-w-prose tablet:mx-0 mb-[60px];
 
 		& > :global(:first-child) {
 			@apply mt-0;
 		}
+
+		& :global(.code-block-wrapper){
+			@apply -ml-4 -mr-4 tablet:ml-0 tablet:mr-0;
+		}
 	}
 
-	article :global(*) {
-		@apply col-[2/3] laptop:col-[1/2];
-	}
-
-	article :global(.code-block-wrapper), article :global(pre) {
-		@apply col-[1/-1] laptop:col-[1/2];
-	}
 
 	nav.toc {
 		@apply h-max sticky top-[20%];
